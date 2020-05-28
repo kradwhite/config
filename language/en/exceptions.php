d5931717253c7ce21d079c5beee7fc02aca4c65e
Aleksandrov Artem <kradwhite@mail.ru>
2020-05-28 20:27:24 +0500

    0.2.1

--- language/en/exceptions.php
+++ language/en/exceptions.php
@@ -1,5 +1,11 @@
 <?php
 
 return [
-	'error' => ['message'],
+    'directory-not-found' => "The '%s ' directory does not exist",
+    'directory-not-directory' => "'%s' is not a directory",
+    'scan-dir-error' => "Error scanning directory '%s'",
+    'locale-is-empty' => 'The language for configuration files is not selected',
+    'directory-create-error' => "Error creating directory '%s'",
+    'file-create-error' => "Error creating file '%s'",
+    'language-texts-not-found' => "The text set for the '%s'file was not found",
 ];
